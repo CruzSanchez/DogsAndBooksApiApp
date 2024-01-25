e12b9116da1b834444a5352322f0ab7912a33848
Cruz Sanchez <cruzsanchez9209@gmail.com>
2024-01-24 19:45:39 -0600

    Add project files.

---
+++ TeachingAPIDemoConsoleUI/Book.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace TeachingAPIDemoConsoleUI
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public DateOnly CreatedDate { get; set; }
+        public string Genre { get; set; } = string.Empty;
+
+        public string ToJson()
+        {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
---
+++ TeachingAPIDemoConsoleUI/Program.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+
+namespace TeachingAPIDemoConsoleUI;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Do you want to view dogs or books?");
+        var userEndpoint = Console.ReadLine();
+
+        HttpClient client = new();
+        HttpRequestMessage request = new()
+        {
+            Method = HttpMethod.Get,
+            RequestUri = new Uri($"https://43a3-2600-6c58-487f-aab8-4869-313b-83ac-a9a4.ngrok-free.app/{userEndpoint}")
+        };
+
+        var response = client.SendAsync(request).Result;
+
+        var body = response.Content.ReadAsStringAsync().Result;
+
+        var books = JsonConvert.DeserializeObject<List<Book>>(body); //List<string> <generics>
+
+        foreach (var book in books)
+        {
+            Console.WriteLine(book.ToJson());
+        }
+    }
+}
